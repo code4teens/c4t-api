6eedc3673f92416aca9fc747d46bae0af8f69c64
Hans Hazairi <hanshazairi@gmail.com>
2021-10-08 00:03:56 +0800

    feat: add GET users/{id} endpoint

--- user.py
+++ user.py
@@ -1,9 +1,25 @@
+from flask import abort
+
 from models import User, UserSchema
 
 
+# users
 def get_all():
     user = User.query.order_by(User.id).all()
     user_schema = UserSchema(many=True)
     data = user_schema.dump(user)
 
     return data
+
+
+# users/<id>
+def get_one(id):
+    user = User.query.filter_by(id=id).one_or_none()
+
+    if user is not None:
+        user_schema = UserSchema()
+        data = user_schema.dump(user)
+
+        return data
+    else:
+        abort(404, f'User not found for ID: {id}')
